e39c1a2f58af6bb303f2eec00c8a07508b289cb9
mperikov <mperikov2@mail.ru>
2024-09-27 01:18:29 +0300

    Сontrollers have been redesigned

---
+++ Controllers/CalculateExpressionController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceCalculator.Models;
+
+namespace ServiceCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateExpressionController : ControllerBase
+    {
+        private readonly ExpAndResContext _context;
+
+        public CalculateExpressionController(ExpAndResContext context)
+        {
+            _context = context;
+        }
+
+        public class Exp
+        {
+            public String? Expression { get; set; }
+        }
+
+        public class Res
+        {
+            public String? Result { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Res>> PostExpAndResItem(Exp InputExp)
+        {
+            Res OutputResult = new Res();
+
+            //
+
+            OutputResult.Result = InputExp.Expression; // заменить на вычисление
+
+            //
+
+            ExpAndResItem expAndResItem = new ExpAndResItem
+            {
+                Result = OutputResult.Result,
+                Expression = InputExp.Expression
+            };
+
+            if (_context.ExpAndResItems == null)
+            {
+                return Problem("Entity set 'ExpAndResContext.ExpAndResItems'  is null.");
+            }
+            _context.ExpAndResItems.Add(expAndResItem);
+            await _context.SaveChangesAsync();
+            return OutputResult;
+        }
+    }
+}
---
+++ Controllers/GetListOfExpressionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiceCalculator.Models;
+
+namespace ServiceCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetListOfExpressionsController : ControllerBase
+    {
+        private readonly ExpAndResContext _context;
+
+        public GetListOfExpressionsController(ExpAndResContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpAndResItem>>> GetExpAndResItems()
+        {
+            if (_context.ExpAndResItems == null)
+            {
+                return NotFound();
+            }
+            return await _context.ExpAndResItems.Reverse().ToListAsync();
+        }
+    }
+}
